ad35ef410ae593b3697daa2e475989d0cceb4ece
parada15 <pb19022@ues.edu.sv>
2026-03-24 13:09:40 -0600

    Comentarios de Metadatos

---
+++ sql-init/V1_20260324_documentacion_metadatos.sql
@@ -0,0 +1,5 @@
+
+--Módulo 4: Documentación y Metadatos (Catálogo)
+
+COMMENT ON TABLE tbl_tecnicos IS 'Registro de personal técnico encargado de la reparación de infraestructuras docentes.'; 
+COMMENT ON COLUMN tbl_pizarras.estado IS 'Indica la disponibilidad operativa del activo: Operativa, En Reparación o Fuera de Servicio.';
