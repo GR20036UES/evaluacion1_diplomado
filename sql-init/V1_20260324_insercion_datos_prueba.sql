a5731b6bc9b7790e36795994f592ea186af1deee
parada15 <pb19022@ues.edu.sv>
2026-03-24 13:24:37 -0600

    Insercion de datos de prueba

---
+++ sql-init/V1_20260324_insercion_datos_prueba.sql
@@ -0,0 +1,12 @@
+-- Insertar una pizarra operativa
+INSERT INTO tbl_pizarras (ubicacion, estado) 
+VALUES 
+('Aula 101 - Facultad de Ciencias', 'Operativa'),
+('Aula 202 - Ingeniería', 'Operativa'),
+('Laboratorio 3 - Sistemas', 'Fuera de Servicio');
+
+INSERT INTO tbl_tecnicos (nombre, especialidad) 
+VALUES 
+('Ana Martínez', 'Hardware Audiovisual'),
+('Carlos López', 'Redes'),
+('María Gómez', 'Electrónica');
